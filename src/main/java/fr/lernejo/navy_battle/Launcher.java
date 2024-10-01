6dc78c240f4b295a9c986447ded881d3d0b25c10
YoniWoo <yonii.atlan@gmail.com>
2024-10-01 10:41:13 +0200

    Partie 1 - Serveur HTTP

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,36 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/ping", new PingHandler());
+        server.setExecutor(Executors.newFixedThreadPool(1));
+        server.start();
+
+        System.out.println("Server started on port " + port);
+    }
+
+    static class PingHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            if("GET".equals(t.getRequestMethod())) {
+                String body = "OK";
+                t.sendResponseHeaders(200, body.length());
+                try (OutputStream os = t.getResponseBody()) {
+                    os.write(body.getBytes());
+                }
+            }
+        }
+    }
+}
