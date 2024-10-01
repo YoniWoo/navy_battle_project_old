74fc07ecde1205e534c19a78994c41780cc972d7
YoniWoo <yonii.atlan@gmail.com>
2024-10-01 18:30:48 +0200

    Partie 3 - Consommer cette API

--- src/main/java/fr/lernejo/navy_battle/GameStartRequest.java
+++ src/main/java/fr/lernejo/navy_battle/GameStartRequest.java
@@ -4,4 +4,13 @@ public class GameStartRequest {
     public String id;
     public String url;
     public String message;
+
+    public GameStartRequest() {
+    }
+
+    public GameStartRequest(String id, String url, String message) {
+        this.id = id;
+        this.url = url;
+        this.message = message;
+    }
 }
--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,5 +1,6 @@
 package fr.lernejo.navy_battle;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -7,14 +8,20 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 
 public class Launcher {
-    public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            throw new IllegalArgumentException("Usage: java -jar NAVY_BATTLE.jar <port>");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length < 1 || args.length > 2) {
+            throw new IllegalArgumentException("One or two arguments are required!");
         }
         int port = Integer.parseInt(args[0]);
+        String ennemyUrl = args.length == 2 ? args[1] : null;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
         server.createContext("/ping", new PingHandler());
@@ -23,7 +30,28 @@ public class Launcher {
         server.start();
 
         System.out.println("Server started on port " + port);
+
+        if (ennemyUrl != null) {
+            sendPostToEnnemy(ennemyUrl, port);
+        }
     }
+    private static void sendPostToEnnemy(String url, int port) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+
+        GameStartRequest myInfo = new GameStartRequest(UUID.randomUUID().toString(), "http://localhost:" + port, "hello");
+        String requestBody = mapper.writeValueAsString(myInfo);
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url + "/api/game/start"))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Réponse de l'adversaire: " + response.statusCode() + " - " + response.body());
+    }
+
 
     static class PingHandler implements HttpHandler {
         @Override
